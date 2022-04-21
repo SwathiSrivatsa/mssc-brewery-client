58776463a51d2c757f222cb1c9d07769fc4e70cf
Swathi <swathibhatkuthyar@gmail.com>
2022-04-21 16:32:28 +0100

    First commit for client

---
+++ src/main/java/com/swa/microservices/mssbreweryclient/web/client/package-info.java
@@ -0,0 +1,2 @@
+package com.swa.microservices.mssbreweryclient.web.client;
+
---
+++ src/test/java/com/swa/microservices/MsscBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swa.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
