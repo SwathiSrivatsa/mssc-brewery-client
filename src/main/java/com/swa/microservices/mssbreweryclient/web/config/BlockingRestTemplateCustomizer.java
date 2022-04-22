f9b4afc6696e201022e38f14a42430902f7d410a
Swathi <swathibhatkuthyar@gmail.com>
2022-04-22 11:00:12 +0100

    Externalizing the values to a property

--- src/main/java/com/swa/microservices/mssbreweryclient/web/config/BlockingRestTemplateCustomizer.java
+++ src/main/java/com/swa/microservices/mssbreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -5,24 +5,43 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+
 @Component
 public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer{
 
+
+	private final Integer maxTotalConnections;
+	private final Integer defaultMaxTotalConnections;
+	private final Integer connectionRequestTimeout;
+	private final Integer socketTimeout;
 	
+	
+	public BlockingRestTemplateCustomizer(@Value("${sfg.maxtotalconnections}") Integer maxTotalConnections, 
+			@Value("${sfg.defaultmaxtotalconnections}") Integer defaultMaxTotalConnections,
+			@Value("${sfg.connectionrequesttimeout}") Integer connectionRequestTimeout, 
+			@Value("${sfg.sockettimeout}") Integer socketTimeout) {
+		
+		this.maxTotalConnections = maxTotalConnections;
+		this.defaultMaxTotalConnections = defaultMaxTotalConnections;
+		this.connectionRequestTimeout = connectionRequestTimeout;
+		this.socketTimeout = socketTimeout;
+	}
+
 	public ClientHttpRequestFactory clientHttpRequestFactory() {
 		
 		PoolingHttpClientConnectionManager connManager=new PoolingHttpClientConnectionManager();
-		connManager.setMaxTotal(100);
-		connManager.setDefaultMaxPerRoute(20);
+		connManager.setMaxTotal(maxTotalConnections);
+		connManager.setDefaultMaxPerRoute(defaultMaxTotalConnections);
 		
-		RequestConfig requestConfig=RequestConfig.custom().setConnectionRequestTimeout(3000)
-				.setSocketTimeout(3000).build();
+		RequestConfig requestConfig=RequestConfig.custom().setConnectionRequestTimeout(connectionRequestTimeout)
+				.setSocketTimeout(socketTimeout).build();
 		
 		CloseableHttpClient httpClient=HttpClients.custom().setConnectionManager(connManager)
 				.setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
